0bf8bec72ea1ad655fbde85da05d01106214f7f1
George <gasiedu@hotmail.co.uk>
2022-06-17 23:50:40 +0100

    Started variable and Tip Calculator

---
+++ Assets/RoutineCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoutineCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(NameRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator NameRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Debug.Log("George");
+        
+    }
+}
